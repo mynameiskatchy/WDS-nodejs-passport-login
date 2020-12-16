67475e5e7b44d8086909e82513cb6e71beae33eb
mynameiskatchy <karchietrinh@gmail.com>
2020-12-15 23:25:58 -0500

    Add bcrypt dependency to hash and compare hashed passwords to make sure app is secure

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const app = express()
 
+const users = []
+
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'Karchie' })
 })
 
+
+
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
+
+
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
 app.post('/register', (req, res) => {
-    res.send("Post register")
+    req.body.email
 })
 
 app.listen(3000)
